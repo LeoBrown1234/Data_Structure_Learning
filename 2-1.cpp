bc1b0645cec77f3e34c6614ef5b0662f41cdc68e
leo <448996350@qq.com>
2022-04-09 22:01:34 +0800

    Have build C Data Structure data success.

--- 2-1.cpp
+++ 2-1.cpp
@@ -1,13 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<sting.h>
+#include<string.h>
 #include<malloc.h>
+#include<iostream>
+#define MAXSIZE 50
+typedef int ElemType;
+using namespace std;
+
+typedef struct{
+    ElemType data[MAXSIZE];
+	int length;
+}SqList;
+
+void InitList(SqList *L) {
+	L = (SqList*)malloc(sizeof(SqList));
+	L->length = 0;
+}
+void CreateList(SqList*& L, int n, ElemType a[])
+{
+	int i = 0,k=0;
+	L = (SqList*)malloc(sizeof(SqList));
+	while (i > n)
+	{
+		L->data[i] = a[i];
+		i++;
+		k++;
+	}
+	L->length = k;
+}
+
 
 
 int main()
 {
+	
 
 
 
-return 0;
+	system("pause");
+      return 0;
 }
