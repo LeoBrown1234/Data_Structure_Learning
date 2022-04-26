4ad3084ce37ae143633b5e4ee8d0e32890890589
leo <448996350@qq.com>
2022-04-26 22:15:38 +0800

    2022/4/26 half completed SqList

--- DS2-1/DS2-1/Algorithm_2_1.cpp
+++ DS2-1/DS2-1/Algorithm_2_1.cpp
@@ -4,7 +4,8 @@
 #define MAX_INT 32767
 #define MIN_INT -32768
 
-void Hello_View() {
+void Hello_View() //尝试的玩意儿
+{ 
 	int Choice;
 	printf("Choice a number you like:\n");
 
@@ -25,17 +26,52 @@ void Hello_View() {
 			printf("Go upstairs.\n");
 			break;
 		}
-
-
-
-
-		
 	}
-
-
-
-	
 }
+
+void  CreateList(SqList*& L, ElemType a[], int n)//1创建顺序表
+{
+	int i,k=0;
+	L = (SqList*)malloc(sizeof(SqList));
+	for (i = 0; i < n; i++)
+	{
+		L->data[i] = a[i];
+		k++;
+	}
+	L->length = k;
+}
+void InitList(SqList*& L) //2初始化顺序表
+{
+	L = (SqList*)malloc(sizeof(SqList));
+	L->length = 0;
+}
+void DestroyList(SqList*& L)//3销毁顺序表
+{
+	free(L);
+}
+bool ListEmpty(SqList* L)//4判断顺序表是否为空
+{
+	return(L->length == 0);
+}
+int ListLength(SqList* L)//5查看顺序表长
+{
+	return(L->length);
+}
+void DispList(SqList* L)//6输出线性表
+{
+	for (int i = 0; i < L->length; i++)
+	{
+		printf("%d",L->data[i]);
+	}
+	printf("\n");
+}
+bool GetElem(SqList* L, int i, ElemType& e);//7获取线性表某个数据元素的值到e
+
+int LocateElem(SqList* L, ElemType e);//8按元素值查找位置
+
+bool ListInsert(SqList*& L, int i, ElemType e);//9插入数据元素
+
+bool ListDelete(SqList*& L, int i, ElemType& e);//10删除数据元素
 
 
 
