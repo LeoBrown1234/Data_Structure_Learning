61743de8101fb3b3bddc8ab5b69cc646da2df4f0
leo <448996350@qq.com>
2022-04-27 18:18:27 +0800

    Completed SqList all

--- DS2-1/DS2-1/Algorithm_2_1.cpp
+++ DS2-1/DS2-1/Algorithm_2_1.cpp
@@ -61,18 +61,62 @@ void DispList(SqList* L)//6输出线性表
 {
 	for (int i = 0; i < L->length; i++)
 	{
-		printf("%d",L->data[i]);
+		printf("%c",L->data[i]);
 	}
 	printf("\n");
 }
-bool GetElem(SqList* L, int i, ElemType& e);//7获取线性表某个数据元素的值到e
-
-int LocateElem(SqList* L, ElemType e);//8按元素值查找位置
-
-bool ListInsert(SqList*& L, int i, ElemType e);//9插入数据元素
-
-bool ListDelete(SqList*& L, int i, ElemType& e);//10删除数据元素
-
+bool GetElem(SqList* L, int i, ElemType& e)//7获取线性表某个数据元素的值到e
+{
+	if (i<1 || i>L->length)
+	{
+		return false;
+	}
+	e = L->data[i - 1];//逻辑位置转换成物理位置
+	return true;
+}
+int LocateElem(SqList* L, ElemType e)//8按元素值查找位置
+{
+	int i = 0;
+	while (i < L->length && L->data[i] != e)
+		i++;
+	if (i > L->length)
+		return 0;
+	else
+		return i + 1;
+}
+bool ListInsert(SqList*& L, int i, ElemType e)//9插入数据元素
+{
+	int j;
+	if (i<1 || i>L->length + 1)
+	{
+		return false;
+	}
+	i--;//逻辑位置转换到物理位置
+	for (j = L->length; j > i; j--)
+	{
+		L->data[j] = L->data[j - 1];//逐个的把元素从最后一个后移，挪出位置
+	}
+		L ->data[i] = e;//让目标位置等于e
+		L->length++;//顺序表表长+1
+		return true;
+	
+} 
+bool ListDelete(SqList*& L, int i, ElemType& e)//10删除数据元素
+{
+	int j;
+	if (i<1 || i>L->length + 1)
+	{
+		return false;
+	}
+	i--;//逻辑序号转换成物理信号
+	e = L->data[i];//取删除的元素
+	for (j =i; j<L->length-1; j++)//从被删除的元素的后一个开始，逐个的前移，因为此时只有L->length-1个元素存在，所以到L->length-1就是到了最后一个元素了
+	{
+		L->data[j] = L->data[j+1];//前一个等于后一个，用j和j+1是因为，j初始值是i，证明一开始j就是i的位置
+	}
+     	L->length--;
+		return true;
+}
 
 
 
